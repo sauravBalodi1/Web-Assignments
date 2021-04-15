4092b795c13eb32eb556a63d344314c47ce3ee70
sauravBalodi1 <sauravbalodi1234@gmail.com>
2021-04-16 01:09:06 +0530

    middleware

--- src/posts/store.js
+++ src/posts/store.js
@@ -1,7 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit';
-import postsReducer from "./postsSlice"
+import {configureStore,applyMiddleware} from '@reduxjs/toolkit';
+import postsReducer from "./postsSlice";
+import logger from "redux-logger"
+const loggerMiddleware=storeApi=>next=>action=>{
+    console.log('dispatching action: ',action)
+    let result=next(action);
+    console.log('next State: ',storeApi.getState())
+    return result;
+}
 export default configureStore({
     reducer:{
         posts:postsReducer,
     }
-})
+},applyMiddleware(loggerMiddleware))
